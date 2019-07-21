b9170530c18244def7469e93ba1b6d357e570bad
zhangzhen <1094705507@qq.com>
2019-07-21 10:53:15 +0800

    添加jpa

---
+++ src/main/java/com/beiwosi/guangzhou/modal/repo/GoodsEntityRepository.java
@@ -0,0 +1,7 @@
+package com.beiwosi.guangzhou.modal.repo;
+
+import com.beiwosi.guangzhou.modal.entity.GoodsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoodsEntityRepository extends JpaRepository<GoodsEntity, Integer> {
+}
