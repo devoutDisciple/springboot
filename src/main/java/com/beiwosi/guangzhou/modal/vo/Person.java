2f6766f705c9009f1b4b480e4c856f2ec1abaa2a
zhangzhen <1094705507@qq.com>
2019-07-23 21:01:08 +0800

    添加logback

--- src/main/java/com/beiwosi/guangzhou/modal/vo/Person.java
+++ src/main/java/com/beiwosi/guangzhou/modal/vo/Person.java
@@ -2,9 +2,11 @@ package com.beiwosi.guangzhou.modal.vo;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
+@PropertySource(value = "classpath:person.properties")
 @Component
 @ConfigurationProperties(prefix = "person")
 public class Person {
--- src/test/java/com/beiwosi/guangzhou/GuangzhouApplicationTests.java
+++ src/test/java/com/beiwosi/guangzhou/GuangzhouApplicationTests.java
@@ -1,23 +1,16 @@
 package com.beiwosi.guangzhou;
 
-import com.beiwosi.guangzhou.modal.entity.GoodsEntity;
 import com.beiwosi.guangzhou.modal.repo.GoodsEntityRepository;
 import com.beiwosi.guangzhou.modal.vo.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GuangzhouApplicationTests {
-
-    @Value("${hello.world}")
-    private String hello;
 
     @Autowired
     Person person;
@@ -27,13 +20,7 @@ public class GuangzhouApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println(hello);
         System.out.println(person);
-        List<GoodsEntity> all = goodsEntityRepository.findAll();
-        for (GoodsEntity goodsEntity: all) {
-            System.out.println(goodsEntity);
-        }
-        System.out.println(all);
     }
 
 }
