a80d66bdf96637ea1fdb9f219e916825cdbe47ac
zhangzhen <1094705507@qq.com>
2019-11-22 01:10:28 +0800

    添加日志文件

--- src/main/java/com/beiwosi/guangzhou/GuangzhouApplication.java
+++ src/main/java/com/beiwosi/guangzhou/GuangzhouApplication.java
@@ -9,4 +9,33 @@ public class GuangzhouApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuangzhouApplication.class, args);
     }
+
+    /**
+     * http请求强制转为https
+     */
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    public Connector httpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8888);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
 }
--- src/test/java/com/beiwosi/guangzhou/GuangzhouApplicationTests.java
+++ src/test/java/com/beiwosi/guangzhou/GuangzhouApplicationTests.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.internet.MimeMessage;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,7 +24,27 @@ public class GuangzhouApplicationTests {
 
     @Test
     public void contextLoads() {
+
         System.out.println(person);
     }
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    @Test
+    public void testSendEmail() {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+            message.setFrom("1094705507@qq.com");
+            message.setTo("228827325@qq.com");
+            message.setSubject("测试主体2222");
+            message.setText("测试内容");
+            javaMailSender.send(mimeMessage);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
 }
